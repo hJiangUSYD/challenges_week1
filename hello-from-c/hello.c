2ceab0aba906f29f9f5f08ab72e7668b683524b4
Anthony Hao <jh991108@gmail.com>
2020-02-25 19:10:39 +1100

    the third change

--- hello-from-c/hello.c
+++ hello-from-c/hello.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello from C !");
-   printf("Hello world");
-   
+   printf("Hello from C!");
+
+
 
    return 0;
 }
